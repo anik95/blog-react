4ed33dc4fa2d44620bf025c9c59de13de208b697
anik95 <mustadirmahmood840@gmail.com>
2018-12-27 12:27:21 +0600

    authentication and sigin/signup for design implemented

--- src/auth/Signin.js
+++ src/auth/Signin.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Grid, Row, Col, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap'
 
 class Signin extends Component {
 	constructor() {
@@ -30,33 +31,50 @@ class Signin extends Component {
         localStorage.setItem('Token', response.data.access_token)
       }).catch(() => console.log("something went wrong"))
 	} 
+	
+	signUpSubmit = () => {
+		this.props.history.push('/signup')
+	}
 
 	renderLoginForm() {
+        const wellStyles = { maxWidth: 600, margin: '10px auto 10px' }
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<label>Email:
-					<input
-						id="email"
-						type="email"
-						value={this.state.email}
-						onChange={this.handleChange}
-					/>
-				</label>
-
-				<label>Password:
-					<input
-						id="password"
-						value={this.state.password}
-						type="password"
-						onChange={this.handleChange}
-					/>
-				</label>
-
-				<input
-					type="submit"
-					value="Submit"
-				/>
-			</form>
+			<div>
+				<Grid>
+					<Row className="show-grid">
+						<Col xs={12} md ={4} mdOffset={4}>
+							<div className="well" style={{margin: '100px 0 0 0'}}>
+								<h4>Sign In</h4>
+								<FormGroup controlId="formBasicText" />
+								<ControlLabel>Email: </ControlLabel>
+									<FormControl 
+										id="email"
+										type="email" 
+										value={this.state.email} 
+										onChange={this.handleChange} 
+									/>
+									<ControlLabel>Password: </ControlLabel>
+									<FormControl 
+										id="password"
+										type="password"
+										onChange={this.handleChange}
+									/>
+									<div style={wellStyles}>    
+										<Button bsStyle="success" bsSize="large" block onClick={this.handleSubmit}>
+											Sign In
+										</Button>
+									</div>
+									<div style={wellStyles}> 
+										<h6>Not yet registered? Sign up now</h6>   
+										<Button bsStyle="primary" bsSize="large" block onClick={this.signUpSubmit}>
+											Sign Up
+										</Button>
+									</div>
+							</div>
+						</Col>
+					</Row>
+				</Grid>
+			</div>
 		)
 	}
 
